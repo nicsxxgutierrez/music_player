bdaac31db06c64203f68212883dd22b8900750f7
Nicolle Gutierrez <130277462+nicsxxgutierrez@users.noreply.github.com>
2023-09-26 16:06:58 +0800

    Update MusicController.php

--- app/Controllers/MusicController.php
+++ app/Controllers/MusicController.php
@@ -4,23 +4,106 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\MusicModel;
-class MainController extends BaseController
+use App\Models\PlaylistModel;
+ 
+class MusicController extends BaseController
 {
-    public function music()
-    {
-        $music = new MusicModel();
-        $data['music'] = $music->findAll();
-        return view ('music', $data);
-    }
     public function index()
     {
-        $data =[
-            'Title' => $this->request->getPost('Title'),
-            'Artist' => $this->request->getPost('Artist'),
-            'File_path' => $this->request->getPost('File_path')
+        $main = new MusicModel();
+        $data['songs'] = $main->findAll();
+        $data['song']= [];
+        return view('music',$data);
+    }
+    public function searchsong()
+    {
+        $searchQuery = $this->request->getVar('search');
+
+        if ($searchQuery) {
+            $main = new MusicModel();
+            $data['song'] = $main->like('Music_Name', $searchQuery)->findAll();
+        }
+        return view('music', $data);
+    }
+    public function addsong()
+    {
+        if($this->request->getMethod() == 'post'){
+            $rules = [
+                    'myfile' => [
+                        'rules' => 'uploaded[myfile]',
+                        'label' => 'My File'
+                    ]
+                ];
+            if($this->validate($rules)){
+                $file = $this->request->getFile('myfile');
+                $filename = pathinfo($file->getName(), PATHINFO_FILENAME);
+                $main = new MusicModel();
+                $data['songs'] = $main->findAll();
+                $data['song'] = [];
+                $datatoadd = [
+                    'Music_Name' => $filename,
+                    'On_Playlist' => "0",
+                ];
+                $main->save($datatoadd);
+                if($file->isValid() && !$file->hasMoved()){
+                    $file->move('./music');
+                }
+                return redirect()->to('/main');
+                exit();
+            }
+        }
+    }
+    public function createplaylist()
+    {
+        if($this->request->getMethod() == 'post'){
+            $play = new PlaylistModel();
+            $data = [
+                'Playlist_Name' => $this->request->getVar('Playlist_Name'),
+                'On_Playlist' => "0"
+            ];
+            $play->save($data);
+            return redirect()->to('/main');
+        }
+    }
+    public function deleteplaylist($ID)
+    {
+        $play = new PlaylistModel();
+        $record = $play->find($ID);
+        if($record != null){
+            $play->delete($ID);
+            return redirect()->to('/main');
+        }
+        else
+        {
+            return "Record not found";
+        }
+    }
+    public function addmusictoplaylist($ID)
+    {
+        $play = new PlaylistModel();
+        $main = new MusicModel();
+        $playlistData = [
+            'plays' => $play->findAll(),
+            'play' => [],
         ];
-        $music = new MusicModel();
-        $music->set($data)->where($data)->update();
-        return redirect()->to('/music');
+        $musicData = [
+            'songs' => $main->findAll(),
+            'song' => [],
+        ];
+        $data = array_merge($playlistData, $musicData);
+        return view('songs', $data);
+    }
+    public function removemusicfromplaylist($ID)
+    {
+        $songs = new MusicModel();
+        $record = $songs->find($ID);
+        if($record != null){
+            $songs->delete($ID);
+            return redirect()->to('/main');
+        }
+        else
+        {
+            return "Record not found";
+        }
     }
 }
